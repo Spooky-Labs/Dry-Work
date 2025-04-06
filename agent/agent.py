6a674c12935a5f0769a3137ffe725de09087ac4e
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2025-04-06 12:12:55 -0700

    Added Agent

---
+++ agent/agent.py
@@ -0,0 +1,23 @@
+import backtrader as bt
+
+class Agent(bt.Strategy):
+    params = (
+        ('fast_period', 10),
+        ('slow_period', 30),
+    )
+    
+    def __init__(self):
+        # Create indicators
+        for d in self.datas:
+            d.fast_ma = bt.indicators.SMA(d, period=self.params.fast_period)
+            d.slow_ma = bt.indicators.SMA(d, period=self.params.slow_period)
+            d.crossover = bt.indicators.CrossOver(d.fast_ma, d.slow_ma)
+    
+    def next(self):
+        for d in self.datas:
+            if not self.getposition(d).size:  # No position
+                if d.crossover > 0:  # Buy signal
+                    size = int(self.broker.getcash() * 0.15 / d.close[0])
+                    self.buy(data=d, size=size)
+            elif d.crossover < 0:  # Sell signal
+                self.close(data=d)
